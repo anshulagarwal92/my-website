8a14e870d1157818d16f9927de61ad5b22c51672
anshul <anshul22.03.92@gmail.com>
2016-03-10 11:45:28 +0530

    updated the config file

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'anktech@123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ec2-54-235-153-179.compute-1.amazonaws.com:5432');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
